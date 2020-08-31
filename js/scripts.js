8a6e6aa902c224bf897ddc60d825c43fb8e0fe07
William Donovan-Seid <wdstwo@live.com>
2020-08-31 10:27:09 -0700

    add html, scripts for journal object and user interface
    co-author: Ophelia Swen <ophelia.swen@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,26 @@
+// Journal Object
+
+function Journal() {
+  this.entrys = [];
+}
+
+Journal.prototype.addEntry = function(entry) {
+  this.entrys.push(entry);
+}
+
+// Entry Object
+
 function Entry() {
   this.title = "";
   this.body = "";
+}
+
+Entry.prototype.addTitle = function(titleToBeAdded) {
+  this.title = titleToBeAdded;
+}
+
+Entry.prototype.addBody = function(bodyToBeAdded) {
+  this.body = bodyToBeAdded;
 }
 
 Entry.prototype.getNumWordEntry = function() {
@@ -30,10 +50,41 @@ Entry.prototype.getTeaser = function() {
   let teaserArray = [];
   for (i=0; i<8; i++) {
     teaserArray.push(wordArray[i]);
-    if (!wordArray[i].includes(endSen)) {
+    if (wordArray[i].includes(endSen)) {
       break;
     }
   }
   let teaser = teaserArray.join(" ");
   return teaser;
 }
+
+// Global Variables
+
+let userJournal = new Journal();
+
+// Display Functions
+
+function displayEntry(entryToDisplay) {
+  let entryList = $("ul#entrys");
+  let htmlForEntryInfo = "";
+  entryToDisplay.entrys.forEach(function(entry) {
+    htmlForEntryInfo += "<li>" + entry.getTeaser() + "</li>"
+  });
+  entryList.html(htmlForEntryInfo);
+}
+
+// User Interface
+
+$(document).ready(function() {
+  $("form#entry-input").submit(function(event) {
+    event.preventDefault();
+    const title = $("input#title").val();
+    const body = $("input#body").val();
+    let journalEntry = new Entry();
+
+    journalEntry.addTitle(title);
+    journalEntry.addBody(body);
+    userJournal.addEntry(journalEntry);
+    displayEntry(userJournal);
+  });
+});
